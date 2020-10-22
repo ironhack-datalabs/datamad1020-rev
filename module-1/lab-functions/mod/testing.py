bd3531204b60a8e9df2cd710f0c5553c2fe2cb30
Rafa <rafadedubra@gmail.com>
2020-10-22 18:55:31 +0200

    solutions of lab-functions

--- module-1/lab-functions/mod/testing.py
+++ module-1/lab-functions/mod/testing.py
@@ -4,15 +4,15 @@ from functools import reduce
 from math import factorial
 from statistics import stdev, mode
 from string import ascii_lowercase, ascii_uppercase, digits
-
-
+​
+​
 def test_greater(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
             self.assertEqual(fn(*self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
@@ -20,14 +20,14 @@ def test_greater(fn):
         a,b = random.randint(-1000,1000),random.randint(-1000,1000)
         suite.addTest(TestKnown([a,b],max([a,b])))
     unittest.TextTestRunner().run(suite)
-
+​
 def test_greatest(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
             self.assertEqual(fn(self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
@@ -35,14 +35,14 @@ def test_greatest(fn):
         arr = [random.randint(-1000,1000) for _ in range(random.randint(10,100))]
         suite.addTest(TestKnown(arr,max(arr)))
     unittest.TextTestRunner().run(suite)
-
+​
 def test_sum(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
             self.assertEqual(fn(self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
@@ -50,15 +50,15 @@ def test_sum(fn):
         arr = [random.randint(-1000,1000) for _ in range(random.randint(10,100))]
         suite.addTest(TestKnown(arr,sum(arr)))
     unittest.TextTestRunner().run(suite)
-
-
+​
+​
 def test_mult(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
             self.assertEqual(fn(self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
@@ -66,32 +66,32 @@ def test_mult(fn):
         arr = [random.randint(-10,10) for _ in range(random.randint(10,100))]
         suite.addTest(TestKnown(arr,reduce(lambda a,b:a*b,arr,1)))
     unittest.TextTestRunner().run(suite)
-
+​
 def test_operations(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
             self.assertEqual(fn(*self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
     for _ in range(100):
-        arr = ([random.randint(-10,10) for _ in range(random.randint(10,100))], random.choice(["+","-"]))
+        arr = ([random.randint(-10,10) for _ in range(random.randint(10,100))], random.choice(["+","*"]))
         def ans(arr,op):
             if op =="+": return sum(arr)
             else: return reduce(lambda a,b:a*b,arr,1)
         suite.addTest(TestKnown(arr,ans(*arr)))
     unittest.TextTestRunner().run(suite)
-
+​
 def test_factorial(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
             self.assertEqual(fn(self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
@@ -99,15 +99,15 @@ def test_factorial(fn):
         n = random.randint(1,100)
         suite.addTest(TestKnown(n,factorial(n)))
     unittest.TextTestRunner().run(suite)
-
-
+​
+​
 def test_unique(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
             self.assertEqual(set(fn(self.input)), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
@@ -115,14 +115,14 @@ def test_unique(fn):
         arr = [random.randint(-100,100) for _ in range(random.randint(10,1000))]
         suite.addTest(TestKnown(arr,set(arr)))
     unittest.TextTestRunner().run(suite)
-
+​
 def test_mode(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
             self.assertEqual(fn(self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
@@ -130,22 +130,22 @@ def test_mode(fn):
         arr = [random.randint(1,25) for _ in range(random.randint(100,125))] + 50 * [random.randint(1,25)]
         suite.addTest(TestKnown(arr,mode(arr)))
     unittest.TextTestRunner().run(suite)
-
+​
 def test_stdev(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
-            self.assertAlmostEqual(fn(self.input), self.output, f"Should be {self.output}")
+            self.assertAlmostEqual(fn(self.input), self.output, delta=5,I’m  msg=f"Should be {self.output}")
     suite = unittest.TestSuite()
     for _ in range(100):
         arr = [random.randint(-1000,1000) for _ in range(random.randint(10,100))]
         suite.addTest(TestKnown(arr,stdev(arr)))
     unittest.TextTestRunner().run(suite)
-
+​
 pangrams = ["Waltz, nymph, for quick jigs vex Bud.",
 "Sphinx of black quartz, judge my vow.",
 "Pack my box with five dozen liquor jugs.",
@@ -161,7 +161,7 @@ pangrams = ["Waltz, nymph, for quick jigs vex Bud.",
 "Watch 'Jeopardy!', Alex Trebek's fun TV quiz game.",
 "By Jove, my quick study of lexicography won a prize!",
 "Waxy and quivering, jocks fumble the pizza."]
-
+​
 def test_pangram(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
@@ -169,28 +169,28 @@ def test_pangram(fn):
             self.input = input
             self.output = output
         def runTest(self):
-            self.assertAlmostEqual(fn(self.input), self.output, f"Should be {self.output}")
+            self.assertEqual(fn(self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
     tests = pangrams + ["".join([random.choice(ascii_lowercase) for _ in range(random.randint(25,100))]) for _ in range(15)] 
     for test in tests:
         suite.addTest(TestKnown(test,set(ascii_lowercase).issubset(set(test.lower()))))
     unittest.TextTestRunner().run(suite)
-
+​
 def test_alpha(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
             super(TestKnown, self).__init__()
             self.input = input
             self.output = output
-
+​
         def runTest(self):
-            self.assertAlmostEqual(fn(self.input), self.output, f"Should be {self.output}")
+            self.assertEqual(fn(self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
     tests = [",".join(["".join([random.choice(ascii_lowercase) for _ in range(random.randint(4,10))]) for _ in range(random.randint(4,25))]) for _ in range(100)] 
     for test in tests:
         suite.addTest(TestKnown(test,",".join(sorted(test.split(",")))))
     unittest.TextTestRunner().run(suite)
-
+​
 def test_pass(fn):
     class TestKnown(unittest.TestCase):
         def __init__(self, input, output):
@@ -199,7 +199,7 @@ def test_pass(fn):
             self.output = output
             
         def runTest(self):
-            self.assertAlmostEqual(fn(self.input), self.output, f"Should be {self.output}")
+            self.assertEqual(fn(self.input), self.output, f"Should be {self.output}")
     suite = unittest.TestSuite()
     check_p = lambda string: sum([len(set(string)&set(c))>0 for c in [ascii_lowercase, ascii_uppercase, digits, "#@!$%&()^*[]{}"]] + [len(string) >= 8]) >= 5
     tests = ["".join([random.choice(ascii_lowercase*3+ascii_uppercase+digits+"#@!$%&()^*[]{}") for _ in range(random.randint(2,16))]) for _ in range(100)] 
