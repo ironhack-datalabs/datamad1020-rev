222e0307a64a3a070ed0842349103dc6e2d6d1f1
Blue Jotas <bluejotas@gmail.com>
2020-10-26 19:48:38 +0100

    changes

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -4,65 +4,73 @@ import numpy as np
 
 #2. Print the NUMPY version and the configuration.
 
-
+# print(np.__version__)
+# print(np.show_config())
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
-
-
 #4. Print a.
 
+a = np.random.random((2, 3, 5))
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
-
-
 #6. Print b.
 
+b = np.full((5, 2, 3), 1)
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(a.size == b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
 
+# You are not able to add two arrays with different shape
 
+#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to variable "c".
 
-#9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
-
+c = np.transpose(b, (1, 2, 0))
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = np.add(a, c)
+print(d)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print(a, d)
+# D is the total sum of 'a+c' because those two arrays can be added to each other as they are the same shape.
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = np.multiply(a, c)
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
-
-
+print(e == a)
+# They equal each other because 'a' is multiplied by 1 (which is the transpose of 'b')
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_max = np.max(d)
+d_min = np.min(d)
+d_mean = np.mean(d)
+print('The max value of d is: ', d_max)
+print('The min value of d is: ', d_min)
+print('The mean value of d is: ', d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
-
+f = np.empty((2, 3, 5))
+print(f)
 
 
 """
@@ -75,7 +83,21 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
+for a in d:
+    for x in a:
+        for i in x:
+            if (i > d_min) and (i < d_mean):
+                f[i] = 25
+            elif (i > d_mean) and (i < d_max):
+                f[i] = 75
+            elif (i == d_mean):
+                f[i] = 50
+            elif (i == d_min):
+                f[i] = 0
+            elif (i == d_max):
+                f[i] = 100
 
+print(f)
 
 
 """
@@ -84,16 +106,13 @@ For instance, if your d is:
 array([[[1.85836099, 1.67064465, 1.62576044, 1.40243961, 1.88454931],
         [1.75354326, 1.69403643, 1.36729252, 1.61415071, 1.12104981],
         [1.72201435, 1.1862918 , 1.87078449, 1.7726778 , 1.88180042]],
-
        [[1.44747908, 1.31673383, 1.02000951, 1.52218947, 1.97066381],
         [1.79129243, 1.74983003, 1.96028037, 1.85166831, 1.65450881],
         [1.18068344, 1.9587381 , 1.00656599, 1.93402165, 1.73514584]]])
-
 Your f should be:
 array([[[ 75.,  75.,  75.,  25.,  75.],
         [ 75.,  75.,  25.,  25.,  25.],
         [ 75.,  25.,  75.,  75.,  75.]],
-
        [[ 25.,  25.,  25.,  25., 100.],
         [ 75.,  75.,  75.,  75.,  75.],
         [ 25.,  75.,   0.,  75.,  75.]]])
@@ -101,14 +120,29 @@ array([[[ 75.,  75.,  75.,  25.,  75.],
 
 
 """
-#18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values 
+#18. Bonus question: instead of using numbers (i.e. 0, 25, 50, 75, and 100), how to use string values
 ("A", "B", "C", "D", and "E") to label the array elements? You are expecting the result to be:
 array([[[ 'D',  'D',  'D',  'B',  'D'],
         [ 'D',  'D',  'B',  'B',  'B'],
         [ 'D',  'B',  'D',  'D',  'D']],
-
        [[ 'B',  'B',  'B',  'B',  'E'],
         [ 'D',  'D',  'D',  'D',  'D'],
         [ 'B',  'D',   'A',  'D', 'D']]])
 Again, you don't need Numpy in this question.
 """
+
+for a in d:
+    for x in a:
+        for i in x:
+            if (i > d_min) and (i < d_mean):
+                f[i] = "B"
+            elif (i > d_mean) and (i < d_max):
+                f[i] = "D"
+            elif (i == d_mean):
+                f[i] = "C"
+            elif (i == d_min):
+                f[i] = "A"
+            elif (i == d_max):
+                f[i] = "E"
+
+print(f)
