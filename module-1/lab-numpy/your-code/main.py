f2825dc5d88504814eb4966652ef2189b9666214
Jose Manuel <j.manuel-3@hotmail.com>
2020-10-26 19:51:34 +0100

    subiendo lab

--- module-1/lab-numpy/your-code/main.py
+++ module-1/lab-numpy/your-code/main.py
@@ -3,65 +3,74 @@
 import numpy as np
 
 #2. Print the NUMPY version and the configuration.
-
-
+print(np.version.version)
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
 
-
+a = np.random.random((2,3,5))
 
 #4. Print a.
 
-
+print(a)
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.ones((5,2,3))
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
 
-
+print(a.size == b.size)
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+print("no se puede porque son de diferente forma")
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
 
-
+c = np.transpose(b, (1,2,0))
+print(c)
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
 
-
+d = np.add(a,c)
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
 
-
+print(a)
+print(d)
+print("como se han podido sumar porque tienen la misma forma, los valores de d son los mismo que los de a pero mas 1")
 
 
 #12. Multiply a and c. Assign the result to e.
 
-
+e = np.multiply(a,c)
+print(e)
 
 #13. Does e equal to a? Why or why not?
 
-
+print("sí, porque se mulltiplica  por unos")
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
 
-
-
+d_min = np.amin(d)
+print(d_min)
+d_max = np.amax(d)
+print(d_max)
+d_mean = np.mean(d)
+print(d_mean)
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
 
+f = np.empty([2, 3, 5])
+print(f)
 
 
 
@@ -75,7 +84,20 @@ In the end, f should have only the following values: 0, 25, 50, 75, and 100.
 Note: you don't have to use Numpy in this question.
 """
 
-
+for i in d:
+        for x in i:
+                for r in x:
+                        if (r > d_min) and (r < d_mean):
+                                print(25)
+                        elif (r > d_mean) and (r < d_max):
+                                print(75)
+                        elif r == d_mean:
+                                print(50)
+                        elif r == d_min:
+                                print(0)
+                        else:
+                                print(100)
+np.append(f, r)
 
 
 """
