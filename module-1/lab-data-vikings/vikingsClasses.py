aa1861617610f17ba31ccc77585e87d912bd0420
jesus perez <firemax366@gmail.com>
2020-10-27 19:41:53 +0100

    sin bonus

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,18 +3,55 @@
 
 
 class Soldier:
-    pass
+
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
+    
+    
+    def attack(self):
+        return self.strength
+    
+    
+    def receiveDamage(self, the_damage):
+        self.health = self.health - the_damage
+        #docstring 
+    
 
 # Viking
 
+class Viking(Soldier):
 
-class Viking:
-    pass
+    def __init__(self, name, health, strength):
+        super().__init__(health, strength) 
+        self.name = name                           
 
+
+    def receiveDamage(self, the_damage):
+        self.health = self.health - the_damage
+        if self.health > 0:
+            return f'{self.name} has received {the_damage} points of damage'
+        else:
+            return f"{self.name} has died in act of combat"
+        #docstring 
+
+    def battleCry(self):
+        return "Odin Owns You All!"
+    
 # Saxon
 
 
-class Saxon:
+class Saxon(Soldier):
+
+    def receiveDamage(self, the_damage):
+        self.health = self.health - the_damage
+        if self.health > 0:
+            return f'A Saxon has received {the_damage} points of damage'
+        else:
+            return f"A Saxon has died in combat"
+        #docstring 
+
+
     pass
 
 # War
