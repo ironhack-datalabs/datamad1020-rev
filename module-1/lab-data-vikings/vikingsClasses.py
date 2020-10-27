fdf64df5e7955cdf8b8abb59703bd55b18bd06db
patri-carrasco <patri.carrasco@gmail.com>
2020-10-27 20:46:06 +0100

    Finished lab-data-vikings

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,6 +1,7 @@
 
 # Soldier
 ### Soldier
+import random
 
 
 class Soldier:
@@ -26,7 +27,7 @@ class Viking(Soldier):
 
     def receiveDamage(self,damage):
         self.health-=damage
-        if self.health == 0:
+        if self.health <= 0:
             return f'{self.name} has died in act of combat'
         else:
             return f'{self.name} has received {damage} points of damage'
@@ -44,7 +45,7 @@ class Saxon(Soldier):
 
     def receiveDamage(self,damage):
         self.health -= damage
-        if self.health == 0:
+        if self.health <= 0:
             return f'A Saxon has died in combat'
         else:
             return f'A Saxon has received {damage} points of damage'
@@ -52,6 +53,53 @@ class Saxon(Soldier):
 
 # War
 
-
+  
 class War:
-    pass
+    
+    def __init__(self):
+        self.vikingArmy = []
+        self.saxonArmy = []
+
+
+    def addViking(self, viking):
+        self.vikingArmy.append(viking)
+
+
+    def addSaxon(self, saxon):
+        self.saxonArmy.append(saxon)
+
+
+    def vikingAttack(self):
+        saxon_index = random.choice(range(0, len(self.saxonArmy))) 
+        saxon = self.saxonArmy[saxon_index]
+        viking = random.choice(self.vikingArmy)
+        saxon.receiveDamage(viking.strength)
+        if saxon.health <= 0:
+            self.saxonArmy.pop(saxon_index)
+            return f'A Saxon has died in combat'
+
+
+    def saxonAttack(self):
+        viking_index = random.choice(range(0,len(self.vikingArmy)))
+        
+        viking = self.vikingArmy[viking_index]
+        saxon = random.choice(self.saxonArmy)
+        viking.receiveDamage(saxon.strength)
+        if viking.health <= 0:
+            self.vikingArmy.pop(viking_index)
+        return f'{viking.name} has received {saxon.strength} points of damage'
+    
+    def showStatus(self):
+        if len(self.vikingArmy) == 0:
+            return f'Saxons have fought for their lives and survive another day...'
+        if len(self.saxonArmy) == 0:
+            return f'Vikings have won the war of the century!'
+        return f'Vikings and Saxons are still in the thick of battle.'
+
+
+
+    
+
+
+    
+
