1411bdd9e2ac78000c32db151c59997b0d398087
pptrv <trv.pepe@gmail.com>
2020-10-27 20:34:21 +0100

    vikings game

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -1,24 +1,33 @@
-
-# Soldier
-
+import random as rd
 
 class Soldier:
-    pass
+    def __init__(self, health, strength):
+        self.health = health
+        self.strength = strength
 
-# Viking
+    def attack(self):
+        return self.strength
 
-
-class Viking:
-    pass
-
-# Saxon
-
-
-class Saxon:
-    pass
-
-# War
-
-
-class War:
-    pass
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        
+class Viking(Soldier):
+    def __init__(self, name, health, strength):
+        Soldier.__init__(self, health, strength)
+        self.name = name
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health > 0:
+            return self.name + " received " +  str(damage) + " points of damage"
+        else:
+            return self.name + " has been brutally killed"
+    def battleCry(self):
+        return "Long live the Vikings!"
+    
+class Saxon(Soldier):
+    def receiveDamage(self, damage):
+        self.health = self.health - damage
+        if self.health > 0:
+            return "A Saxon has received " +  str(damage) + " points of damage"
+        else:
+            return "A Saxon has been brutally killed"
