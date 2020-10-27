7c122417596e70b5744183524b061df71217e5bf
Juan Perez <jperezllorente@hotmail.com>
2020-10-27 17:32:13 +0100

    lab-data-viking

--- module-1/lab-data-vikings/vikingsClasses.py
+++ module-1/lab-data-vikings/vikingsClasses.py
@@ -3,19 +3,71 @@
 
 
 class Soldier:
-    pass
+
+    def __init__(self,health,strength):
+
+        self.health = health
+
+        self.strength = strength
+
+    def attack(self):
+
+        return self.strength
+
+    def receiveDamage(self,damage):
+
+        self.health = self.health - damage
 
 # Viking
 
 
-class Viking:
-    pass
+class Viking(Soldier):
+
+    def __init__ (self,name, health, strength):
+
+        self.name = name
+    
+        super(). __init__ (health, strength)
+
+        
+    def receiveDamage(self,damage):
+
+        self.health = self.health - damage
+
+        if self.health > 0:
+            
+            return f"{self.name} has received {damage} points of damage"
+  
+        else:
+
+            return f"{self.name} has died in act of combat"
+    
+    def battleCry(self):
+
+        return "Odin Owns You All"
+    
 
 # Saxon
 
 
-class Saxon:
-    pass
+class Saxon(Soldier):
+
+    def __int__ (self,health,strength):
+
+        super.__int__(health,strength)
+    
+    def receiveDamage(self,damage):
+
+        self.health = self.health - damage
+
+        if self.health > 0:
+
+            return f"A Saxon has received {damage} points of damage"
+
+        else:
+
+            return "A Saxon has died in combat"
+    
 
 # War
 
