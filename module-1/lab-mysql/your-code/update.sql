20d1a341d443b417afd09da08c95b01652a857da
patri-carrasco <patri.carrasco@gmail.com>
2020-11-04 20:04:52 +0100

    Create update.sql

---
+++ module-1/lab-mysql/your-code/update.sql
@@ -0,0 +1,15 @@
+USE car_dealership;
+SELECT * FROM customer_id;
+
+UPDATE customer_id 
+SET email = 'ppicasso@gmail.com'
+WHERE customer_id= 10001;
+
+UPDATE customer_id 
+SET email = 'lincoln@us.gov'
+WHERE customer_id= 20001;
+
+UPDATE customer_id 
+SET email = 'hello@napoleon.me'
+WHERE customer_id= 30001;
+
