efc1209269e5ab72602d35dbb64ca4c319432fb0
Ana Murillo <ana_murillo_amores@hotmail.com>
2020-11-04 18:18:21 +0100

    minor fixes

--- module-1/lab-mysql/your-code/Delete&Update.sql
+++ module-1/lab-mysql/your-code/Delete&Update.sql
@@ -1,5 +1,5 @@
 UPDATE `lab_mysql`.`Salespersons` SET `Store` = 'Miami' WHERE (`idSalespersons` = '5');
-UPDATE `lab_mysql`.`Customer` SET `E-mail` = 'ppicasso@gmail.com' WHERE (`idCustomer` = '0');
-UPDATE `lab_mysql`.`Customer` SET `E-mail` = 'unamuno@miguel.usal' WHERE (`idCustomer` = '1');
-UPDATE `lab_mysql`.`Customer` SET `E-mail` = 'malasaña@manuela.me' WHERE (`idCustomer` = '2');
-DELETE FROM `lab_mysql`.`Car` WHERE (`idCar` = '4');
+UPDATE `lab_mysql`.`Customers` SET `E-mail` = 'ppicasso@gmail.com' WHERE (`idCustomer` = '0');
+UPDATE `lab_mysql`.`Customers` SET `E-mail` = 'unamuno@miguel.usal' WHERE (`idCustomer` = '1');
+UPDATE `lab_mysql`.`Customers` SET `E-mail` = 'malasaña@manuela.me' WHERE (`idCustomer` = '2');
+DELETE FROM `lab_mysql`.`Cars` WHERE (`idCars` = '4');
