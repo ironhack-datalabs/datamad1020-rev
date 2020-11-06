6a15319ca95209cd67b5bdb76a6b7729ac341d89
jesus perez <firemax366@gmail.com>
2020-11-06 12:36:17 +0100

    arreglado el challenge 4

--- module-1/lab-mysql-select/your-code/Challenge 4.sql
+++ module-1/lab-mysql-select/your-code/Challenge 4.sql
@@ -3,22 +3,10 @@ USE publications;
 SELECT a.au_id AS 'AUTHOR ID', au_lname AS 'LAST NAME', au_fname AS 'FIRST NAME',
 COALESCE(SUM(ytd_sales),0) AS TOTAL
 FROM AUTHORS AS a
-INNER JOIN titleauthor AS ta
-ON a.au_id = ta.au_id
-INNER JOIN titles AS tl
-ON ta.title_id = tl.title_id
-INNER JOIN publishers AS ps
-ON tl.pub_id = ps.pub_id
-UNION
-SELECT a.au_id AS 'AUTHOR ID', au_lname AS 'LAST NAME', au_fname AS 'FIRST NAME',
-COALESCE(SUM(ytd_sales),0) AS TOTAL
-FROM AUTHORS AS a
 LEFT JOIN titleauthor AS ta
 ON a.au_id = ta.au_id
 LEFT JOIN titles AS tl
 ON ta.title_id = tl.title_id
-LEFT JOIN publishers AS ps
-ON tl.pub_id = ps.pub_id
 GROUP BY (a.au_id)
 ORDER BY total DESC
 LIMIT 23;
